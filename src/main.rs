a3adae4562bb884a5e9c6de2871bbce106c4c491
TheCheesyWiggle <74103207+TheCheesyWiggle@users.noreply.github.com>
2021-08-26 16:56:31 +0100

    fixed some typos but userinput conversion isnt working

--- src/main.rs
+++ src/main.rs
@@ -7,52 +7,54 @@ fn main() {
     rules();
 
     println!("ROCK! PAPER! SCISSORS! SHOOT!");
-    result(aiChoice(),userChoice());
+    result(ai_choice(),user_choice());
 }
 
 fn rules(){
     println!("--------------------------------------------------\nRULES\n--------------------------------------------------\nEnter: \n\t R = Rock \n\t P = Paper \n\t S = Scissors\n-------------------------------------------------- \nThe computer will reveal its choice and the winner");
 }
 
-fn userInput()-> String{
+fn user_input()-> String{
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("error: unable to read user input");
     return input
 }
 
-fn userChoice()->u8{
+fn user_choice()->u8{
     //defines variables
-    let mut input = userInput();
+    let mut input = user_input();
     let mut choice:u8 = 3;
 
-    //sets outcomes to variables
-    let r = String::from("r");
-    let p = String::from("p");
-    let s = String::from("s");
 
-    //switch statment
-    match input {
-        r => choice = 0,
-        p => choice = 1,
-        s => choice = 2,
-        other => println!("Error {} is invalid", other),
+
+    //if statement too convert the user choice into the same data type as ai
+    if input == "r" {
+        choice = 0;
     }
+    else if  input == "p"{
+        choice = 1;
+    }
+    else if  input == "s"{
+        choice = 2;
+    }
+    else{
+        println!("Error");
+    }
+    println!("{},{}",choice,input);
     //returns choice
     return choice
 }
 
-fn aiChoice()->u8{
+fn ai_choice()->u8{
     //specify thread
     let mut rng = rand::thread_rng();
     //random number
     let choice: u8 = rng.gen_range(0..2);
-    println!("{}",choice);
-    aiOutput(choice);
     return choice
 }
 
-fn aiOutput(aiChoice:u8){
-    match aiChoice {
+fn ai_output(ai_choice:u8){
+    match ai_choice {
         0 => println!("Computer chose Rock"),
         1 => println!("Computer chose Paper"),
         2 => println!("Computer chose Scissors"),
@@ -61,41 +63,42 @@ fn aiOutput(aiChoice:u8){
 
 }
 
-fn result(aiChoice: u8 , userChoice: u8) {
+fn result(ai_choice: u8 , user_choice: u8) {
+    ai_output(ai_choice);
     let mut win:u8 = 0;
 
-    if userChoice == aiChoice {
+    if user_choice == ai_choice {
         win = 3;
     }
     //>>>>>USER CHOOSES ROCK<<<<<//
-    else if userChoice == 0{
-        if aiChoice == 1{
+    else if user_choice == 0{
+        if ai_choice == 1{
             win = 1;
         }
-        else if aiChoice == 2{
+        else if ai_choice == 2{
             win = 2;
         }
     }
     //>>>>>USER CHOOSES PAPER<<<<<//
-    else if userChoice == 1{
-        if aiChoice == 0{
+    else if user_choice == 1{
+        if ai_choice == 0{
             win = 2;
         }
-        else if aiChoice == 2{
+        else if ai_choice == 2{
             win = 1;
         }
     }
     //>>>>>USER CHOOSES SCISSORS<<<<<//
-    else if userChoice == 2{
-        if aiChoice == 0{
+    else if user_choice == 2{
+        if ai_choice == 0{
             win = 1;
         }
-        else if aiChoice == 1{
+        else if ai_choice == 1{
             win = 2;
         }
     }
 
-    println!("{},{}",aiChoice,userChoice);
+    println!("{},{}",ai_choice,user_choice);
 
     match win{
         1 => println!("Well Done you have won!"),
